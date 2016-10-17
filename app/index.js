a9458ebc577b32c475ff6a0ba3c608fcf9182edf
co2-git <francoisrvespa@gmail.com>
2016-10-17 17:06:11 -0500

    export version

--- app/index.js
+++ app/index.js
@@ -4,3 +4,5 @@ export {default as Model} from './lib/Model';
 export {default as Schema} from './lib/Schema';
 import * as Type from './lib/Type';
 export {Type};
+import packageJSON from '../package.json';
+export const {version} = packageJSON;
